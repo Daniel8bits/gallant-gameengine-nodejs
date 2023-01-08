500e2aa1a62998583af07e5c2ca4cfa59a4a4b95
Daniel8bits <daniel.sftm@outlook.com>
2023-01-08 16:28:39 -0300

    .

--- src/core/GameLoop.ts
+++ src/core/GameLoop.ts
@@ -20,8 +20,6 @@ class GameLoop {
         time *= 0.001;  // seconds;
         const delta = time - this._then;
         this._then = time;
-        const fps = 1 / delta;             // compute frames per second
-        document.querySelector("#fps span").textContent = fps.toFixed(1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
         InputManager.update()
         this._gameCore.update(time, delta);
